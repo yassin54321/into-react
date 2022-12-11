d3f30b5dea3dc6baa9801d3fc4ddbc60d8fbac35
yassin54321 <yassin.gharbi5@gmail.com>
2022-12-11 17:06:29 +0100

    intro-react

---
+++ src/App.js
@@ -0,0 +1,49 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container, Form, Nav, Navbar } from 'react-bootstrap';
+function App() {
+  return (
+    <div className="App">
+    <>
+
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+    </>
+
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+
+export default BasicExample;
+    </div>
+  );
+}
+
+export default App;
